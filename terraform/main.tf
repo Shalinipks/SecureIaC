a090bbb047ad384e85bb1c21b25685446c3bf563
Shalinipks <shalini.pks23@gmail.com>
2026-08-12 20:04:05 +0530

    Fix Gitleaks executable path

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,12 @@ resource "aws_security_group" "insecure_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
+}
+resource "aws_instance" "secure_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.insecure_sg.id]
 }
